1fe3d134a2fc4a4edee1eda32ab93be5f80612db
Jenny Evertsson <jentap@gmail.com>
2024-11-19 11:37:48 +0100

    Assignment Tuesday Week47

---
+++ AssignmentWk47/BusinessLogic/BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,97 @@
+﻿
+using System.Collections.Generic;
+
+namespace BusinessLogic
+{
+    public class BusinessLogics
+    {
+        private static decimal musicVAT = 0.06M;
+        private static decimal foodVAT = 0.12M;
+        private static decimal alcoholVAT = 0.25M;
+        private static decimal calculatedVAT = 0;
+        public static decimal ReturnVAT(string typeOfPurchase, decimal priceOfItem )
+        {
+            switch (typeOfPurchase)
+            {
+                case "music":
+                    calculatedVAT = priceOfItem * musicVAT;
+                    return calculatedVAT;
+                    break;
+                case "food":
+                    calculatedVAT = priceOfItem * foodVAT;
+                    return calculatedVAT;
+                    break;
+                case "alcohol":
+                    calculatedVAT = priceOfItem * alcoholVAT;
+                    return calculatedVAT;
+                    break;
+                default:
+                    return 0;
+            }
+        }
+
+        public static int MembershipFee (int memberAge, bool premium)
+        {
+            int payable = 0;
+            if (premium)
+            {
+                payable += 300;
+            }
+            if (memberAge< 19 || memberAge > 64)
+            {
+                payable += 2200;
+            } else
+            {
+                payable += 3100;
+            }
+            return payable;
+        }
+        public static double MyCalculator(string methodOperator, double valueOne, double valueTwo)
+        {
+            switch (methodOperator)
+            {
+                case "divide":
+                    return valueOne / valueTwo;
+                    break;
+                case "multiply":
+                    return valueOne * valueTwo;
+                    break;
+                case "subtract":
+                    return valueOne - valueTwo;
+                    break;
+                case "add":
+                    return valueOne + valueTwo;
+                    break;
+                default:
+                    return 0;
+            }
+        }
+
+        // valit input values are: help, add, delete, edit, find, list
+        public static void MenuOptions(string input)
+        {
+            switch (input)
+            {
+                case "help":
+                    Console.WriteLine("This method should output a screen print when you enter one of the following parameters: help, add, delete, edit, find, list");
+                    break;
+                case "add":
+                    Console.WriteLine("You wanted to add a menu item");
+                    break;
+                case "delete":
+                    Console.WriteLine("Which menu item do you wish to delete?");
+                    break;
+                case "edit":
+                    Console.WriteLine("Now editing menu item");
+                    break;
+                case "find":
+                    Console.WriteLine("I have found the menu item you were looking for");
+                    break;
+                case "list":
+                    Console.WriteLine("These are the menu items I have available: Tea ; Coffee ; Juice");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ AssignmentWk47/BusinessLogic/BusinessLogic/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Make a value-added tax method for a café business. The method should
+take a string value indicating one of three product options and a decimal
+value representing the price of the product. The method should return the
+value added tax (VAT) calculated.
+The café on occasions sells music entertainment. The VAT is 6 percent.
+The café sells food. The VAT is 12 percent.
+The café sells alcohol. The VAT is 25 percent.
+ * */
+
+using BusinessLogic;
+using System.Net.WebSockets;
+// Assignment 1
+Console.WriteLine("Business Logic, Assignment week 47 - 1: VAT calculations ");
+
+decimal calculatedVAT = BusinessLogics.ReturnVAT("music", (decimal) 12.14);
+Console.WriteLine("calculated VAT on 12.14 for music " + calculatedVAT);
+
+calculatedVAT = BusinessLogics.ReturnVAT("food", (decimal) 12.14);
+Console.WriteLine("calculated VAT on 12.14 for food: " + calculatedVAT);
+
+calculatedVAT = BusinessLogics.ReturnVAT("alcohol", (decimal)12.14);
+Console.WriteLine("calculated VAT on 12.14 for food: " + calculatedVAT);
+
+// Should return 0, not a defined VAT
+calculatedVAT = BusinessLogics.ReturnVAT("drink", (decimal)12.14);
+Console.WriteLine("calculated VAT on 12.14 for drink: " + calculatedVAT);
+
+
+
+/* 
+ * Make a method calculating the price when signing up for a gym
+membership. The method should take an int value indicating the age of
+the signee and a bool value indicating a wish for premium membership.
+Persons below the age of 19 and above the age of 64 pay 2200 for their
+membership. All else pay 3100. The premium membership is a straight 300
+amount on top of what the signee already will pay. The method should
+return the full amount payable for the signee.
+ */
+Console.WriteLine("Business Logic, Assignment week 47 - 2: Gym membership ");
+//  Change the following two parameters to test.
+int ageOfMEmber = 73;
+bool premium = true;
+Console.WriteLine("Membership fee for "+ageOfMEmber+" year old. Premium = "+ premium);
+Console.WriteLine(BusinessLogics.MembershipFee(ageOfMEmber, premium));
+
+/*
+ * Use a switch statement to make a calculator method that takes a string
+value indicating the mathematical operation and two double values as
+parameters. The method returns the result of the operation, which should
+at least include adding, subtracting, multiplying and dividing.
+4.
+ */
+Console.WriteLine("Business Logic, Assignment week 47 - 3: A Calculator ");
+// Change the following three parameters to test. Proper terms for methodOperator are: divide, add, subtract, multiply
+string methodOperator = "divide";
+double valueOne = 3;
+double valueTwo = 4;
+double assignment3Result = BusinessLogics.MyCalculator(methodOperator, valueOne, valueTwo);
+Console.WriteLine("The result from " + methodOperator + ", " + valueOne + ", " + valueTwo +" is: "+ assignment3Result);
+
+methodOperator = "add";
+assignment3Result = BusinessLogics.MyCalculator(methodOperator, valueOne, valueTwo);
+Console.WriteLine("The result from " + methodOperator + ", " + valueOne + ", " + valueTwo + " is: " + assignment3Result);
+
+/* 
+ * Make a menu options method that takes one of the following options as a
+string value parameter:
+- help, add, delete, edit, find, list
+The method should not return any value, but for the option chosen, print
+out a unique and appropriate message using Console.WriteLine().
+*/
+Console.WriteLine("Business Logic, Assignment week 47 - 4: Menu options");
+// Change the following parameter to test
+string menuInput = "help";
+BusinessLogics.MenuOptions(menuInput);
+menuInput = "edit";
+BusinessLogics.MenuOptions(menuInput);
+menuInput = "list";
+BusinessLogics.MenuOptions(menuInput);
